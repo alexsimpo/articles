91c7c128d0b51b419420d1c77e76c821bdb5baef
Alex Simpson <alxsimpson@gmail.com>
2020-08-07 20:35:05 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+//Check connection
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Check DB errors
+db.on('error', (err) => {
+    console.log(err);
+});
+
+// Init app
+const app = express();
+
+// Bring in models
+let Article = require('./models/article');
+
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Parse
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Home route
+app.get('/', (req, res) => {
+    Article.find({}, (err, articles) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        }
+    });
+});
+
+// Add Route
+app.get('/articles/add', (req, res) => {
+    res.render('add_article', {
+        title: 'Add Article'
+    });
+})
+
+// Add submit POST route
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+// Start server
+app.listen(3001, () => {
+    console.log('Server started in Port 3001...');
+});
